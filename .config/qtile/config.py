afbfdce50cee1020f0af3bac042bb0e29682104c
Tristan Snow <tristansnowcobb@gmail.com>
2023-09-15 22:08:58 -0400

    Moved autostart.sh.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -20,7 +20,7 @@ terminal = "alacritty"
 
 @hook.subscribe.startup_once
 def autostart():
-    home = os.path.expanduser('~/.config/qtile/scripts/autostart.sh')
+    home = os.path.expanduser('~/.config/qtile/autostart.sh')
     subprocess.Popen([home])
 
 keys = [
